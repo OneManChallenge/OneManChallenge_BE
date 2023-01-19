649f4438b1772ed89877fd695d11ad1e2420afdb
SeDevel <ohhojin25@naver.com>
2023-01-19 23:57:16 +0900

    init : 초기세팅
    - 템플릿
    - gitignore
    - springboot project 기본 파일

---
+++ src/main/java/com/hanghae/onemanitnews/OneManBeApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.onemanitnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneManBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneManBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanghae/onemanitnews/OneManBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.onemanitnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneManBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
