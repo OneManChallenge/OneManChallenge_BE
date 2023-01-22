ad8818f254525e11b23e562dae704ea57ab8aa82
SeDevel <ohhojin25@naver.com>
2023-01-23 01:59:06 +0900

    feat : 검색 기능 구현(#3)
    - 테스트코드는 ELK연동 완료 후 진행 예정

--- src/main/java/com/hanghae/onemanitnews/entity/News.java
+++ src/main/java/com/hanghae/onemanitnews/entity/News.java
@@ -4,17 +4,26 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@EntityListeners(AuditingEntityListener.class)
 @SQLDelete(sql = "UPDATE news SET deleted = true WHERE news_id = ?") //soft delete 적용
 @Where(clause = "deleted = false") //soft delete 적용하여, 조회 시 자동으로 delete = false 조건절 추가
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class News {
@@ -36,10 +45,10 @@ public class News {
 	@Column(nullable = false, name = "article_date")
 	private LocalDateTime articleDate;
 
-	@Column(nullable = false, length = 100)
+	@Column(nullable = false, name = "img_url", length = 300)
 	private String imgUrl;
 
-	@Column(nullable = false, length = 100)
+	@Column(nullable = false, name = "main_url", length = 100)
 	private String mainUrl;
 
 	@Column(nullable = false)
@@ -47,4 +56,27 @@ public class News {
 
 	@Column(nullable = false)
 	private Boolean deleted = Boolean.FALSE; //삭제유무 기본값 false
+
+	@LastModifiedDate
+	@Column(name = "modification_date", nullable = false) //Logstash 동기화 위해 사용
+	private LocalDateTime modificationDate;
+
+	@PrePersist
+	private void onPrePersist() {
+		this.modificationDate = LocalDateTime.now().withNano(0);
+	}
+
+	@Builder
+	public News(Long newsId, String division, String title,
+		String content, LocalDateTime articleDate, String imgUrl,
+		String mainUrl, Long views) {
+		this.newsId = newsId;
+		this.division = division;
+		this.title = title;
+		this.content = content;
+		this.articleDate = articleDate;
+		this.imgUrl = imgUrl;
+		this.mainUrl = mainUrl;
+		this.views = views;
+	}
 }
--- src/main/java/com/hanghae/onemanitnews/repository/NewsRepository.java
+++ src/main/java/com/hanghae/onemanitnews/repository/NewsRepository.java
@@ -1,8 +1,13 @@
 package com.hanghae.onemanitnews.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hanghae.onemanitnews.entity.News;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
+	Page<News> findAllByTitleContains(String search, Pageable pageable);
+
+	Page<News> findAllByContentContains(String search, Pageable pageable);
 }
