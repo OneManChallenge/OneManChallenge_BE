5816679bc87690a186ba539a51a4a89ad6a9dbd1
SeDevel <ohhojin25@naver.com>
2023-01-23 02:02:04 +0900

    refactor : 생성/수정일자 컬럼명 수정(#3)
    - ELK 공식 사이트 참고 후 명칭 변경
    https://www.elastic.co/kr/blog/how-to-keep-elasticsearch-synchronized-with-a-relational-database-using-logstash

--- src/main/java/com/hanghae/onemanitnews/entity/Timestamped.java
+++ src/main/java/com/hanghae/onemanitnews/entity/Timestamped.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -17,10 +18,22 @@ import lombok.Getter;
 @EntityListeners(AuditingEntityListener.class)
 public class Timestamped {
 	@CreatedDate
-	@Column(name = "creation_date", nullable = false)
-	private LocalDateTime creationDate;
+	@Column(name = "insertion_date", nullable = true)
+	private LocalDateTime insertionDate;
 
 	@LastModifiedDate
-	@Column(name = "modification_date", nullable = false)
+	@Column(name = "modification_date", nullable = true)
 	private LocalDateTime modificationDate;
+
+	/**
+	 @PrePersist : 해당 엔티티 저장하기 전 실행
+	 - onPrePersist() : 생성일자 내 나노초 제거함
+	 사용 전 : 2022-12-25T01:23:53.73487
+	 사용 후 : 2022-12-25T01:23:53
+	 */
+	@PrePersist
+	private void onPrePersist() {
+		this.insertionDate = LocalDateTime.now().withNano(0); //나노초 9자 제거
+		this.modificationDate = LocalDateTime.now().withNano(0);
+	}
 }
