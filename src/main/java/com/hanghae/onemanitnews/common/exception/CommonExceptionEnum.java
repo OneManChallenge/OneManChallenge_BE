1e5f7cee07cc8a8794908abedcc3c19b9d813c42
SeDevel <ohhojin25@naver.com>
2023-01-25 17:43:18 +0900

    feat : 회원가입 예외 Enum 추가(#8)

--- src/main/java/com/hanghae/onemanitnews/common/exception/CommonExceptionEnum.java
+++ src/main/java/com/hanghae/onemanitnews/common/exception/CommonExceptionEnum.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 @Getter
 public enum CommonExceptionEnum {
 	/* News 예외 메시지 */
-	NEWS_NOT_FOUND_MSG("뉴스가 검색되지 않습니다.");
+	NEWS_NOT_FOUND_MSG("뉴스가 검색되지 않습니다."),
+
+	/* Member 예외 메시지 */
+	DUPLICATE_EMAIL("이미 가입한 Email입니다."),
+	MEMBER_NOT_FOUND("회원이 확인되지 않습니다.");
 
 	private final String msg;
 
